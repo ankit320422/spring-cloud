2ffbb127a8719b243c03cec8ca21fbe0642727d1
ankit.singh <ankit.singh@centilytics.com>
2021-04-18 16:27:24 +0530

    currency porject added in soring cloud .....

--- limit-services/src/main/java/com/springcloud/limitservices/LimitServicesApplication.java
+++ limit-services/src/main/java/com/springcloud/limitservices/LimitServicesApplication.java
@@ -2,12 +2,16 @@ package com.springcloud.limitservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationProperties("")
 public class LimitServicesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LimitServicesApplication.class, args);
 	}
 
--- limit-services/src/main/java/com/springcloud/limitservices/controller/LimitController.java
+++ limit-services/src/main/java/com/springcloud/limitservices/controller/LimitController.java
@@ -1,6 +1,7 @@
 package com.springcloud.limitservices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class LimitController {
 
 	@Autowired
 	ConfigureProperties configureProperties;
+
 
 	@GetMapping("/get")
 	public LimitModel get() {
